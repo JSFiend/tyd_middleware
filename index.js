56c38b9775af11aa4da0be44fe5867dfea1a97a8
combineli <combineli@tencent.com>
2019-06-25 14:40:11 +0800

    feature： 响应 404、 500 请求

--- index.js
+++ index.js
@@ -1,22 +1,31 @@
+const finalhandler = require('finalhandler');
 
 class TydMiddleware {
 
-    constructor() {
-        this.middlewares = [];
-        this.inject = this.inject.bind(this);
-    }
+  constructor() {
+    this.middlewares = [];
+    this.inject = this.inject.bind(this);
+  }
 
-    use(...middlewares) {
-        middlewares.forEach((middleware) =>{
-            if (typeof middleware !== 'function') throw new Error('中间件必须是函数');
-        })
-        this.middlewares = [...this.middlewares, ...middlewares];
-    }
+  use(...middlewares) {
+    middlewares.forEach((middleware) => {
+      if (typeof middleware !== 'function') throw new Error('中间件必须是函数');
+    });
+    this.middlewares = [...this.middlewares, ...middlewares];
+  }
 
-    // 作为中间件注入灵魂
-    inject(req, res) {
-        this.middlewares.forEach(middleware => middleware(req, res));
+  // 作为中间件注入灵魂
+  inject(req, res) {
+    const callback = finalhandler(req, res);
+    try {
+      this.middlewares.forEach(middleware => middleware(req, res, callback));
+      // 处理 404 请求
+      callback();
+    } catch (err) {
+      // 处理 500 请求
+      callback(err);
     }
+  }
 }
 
 module.exports = TydMiddleware;
--- test.js
+++ test.js
@@ -3,7 +3,8 @@ const assert = require('assert');
 const http = require('http');
 const axios = require('axios');
 const TydMiddleware = require('./index');
-let tydMiddleware = undefined;
+let tydMiddleware;
+let server;
 
 describe('think you do 中间价管理器', function() {
     it('实例化管理器', function() {
@@ -11,14 +12,38 @@ describe('think you do 中间价管理器', function() {
         tydMiddleware.use(function (req, res) {}, function (req, res) {});
         assert(tydMiddleware.middlewares.length === 2, '实例化管理器错误，不能添加中间件');
     });
-    it('http 服务注入 管理器', async function() {
-        const server = http.createServer().listen(3000);
-        server.on('request', tydMiddleware.inject);
-        tydMiddleware.use(function(req, res){ res.end(req.url) });
-        const path = '/aa?name=combineli'
-        const res = await axios.get('http://localhost:3000' + path);
-        assert(res.data === path);
-        server.close();
-    })
-})
+    describe('http 服务注入 管理器', function () {
+        beforeEach(function () {
+            tydMiddleware = new TydMiddleware();
+            server = http.createServer().listen(3000);
+            server.on('request', tydMiddleware.inject);
+        });
+        it('get 请求', async function() {
+            tydMiddleware.use(function(req, res){ res.end(req.url) });
+            const path = '/aa?name=combineli';
+            const res = await axios.get('http://localhost:3000' + path);
+            assert(res.data === path);
+        });
+        it('404 请求', async function () {
+            const path = '/aa?name=combineli';
+            try {
+                const res = await axios.get('http://localhost:3000' + path);
+            } catch (err) {
+                assert(err.response.status === 404, `期待状态码返回404，实际是${err.response.status}`);
+            }
+        });
+        it('500 请求', async function () {
+            tydMiddleware.use(function(req, res, next) { res.end(abc)});
+            try {
+                const res = await axios.get('http://localhost:3000');
+            } catch (err) {
+                assert(err.response.status === 500, `期待状态码返回500，实际是:${err.response.status}, message:${err}`);
+            }
+        });
+        afterEach(function () {
+            server.close();
+        })
+    });
+
+});
 
